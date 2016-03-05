30611a66090c6f97994ef4a509ec5dd7263ca73d
NikitaSadinidhiVijayan <sv.nikita91@yahoo.com>
2016-03-05 11:01:52 -0800

    Modified deals

--- internationaltrade/deals.php
+++ internationaltrade/deals.php
@@ -128,9 +128,10 @@
 									</select>
 									
 									<select class = "span5" id = "time_restricted" name="time_restricted"> 
-										<option value= "time_restricted">1</option>
-										<option value= "Yes">2</option>
-										<option value= "No">3</option>
+										<option value= "Stage_Discount">Stage Discount</option>
+										<option value= "1">1</option>
+										<option value= "2">2</option>
+										<option value= "3">3</option>
 									</select>
 								</div>
 							</div>
