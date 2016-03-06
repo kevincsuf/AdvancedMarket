8a5782c26415156ae3ac25bb0f7e3b80c165db31
NikitaSadinidhiVijayan <sv.nikita91@yahoo.com>
2016-03-06 12:08:11 -0800

    Register_lib added sql insert query for buyer
    Kevin please refer the latest code

--- internationaltrade/register_lib.php
+++ internationaltrade/register_lib.php
@@ -1,42 +1,50 @@
 <?php 
-/*
-				$regtype; // "seler"
-				$seller_email_value = "";
-				$seller_pwd_value = "";
-				$seller_first_name_value = "";
-				$seller_last_name_value = "";
-				$seller_buss_name_value = "";
-				$seller_addr_value = "";
-				$seller_rev_msg_yes = "";
-				$seller_rev_msg_no = "";
-				$seller_category_food = "";
-				$seller_category_electronics = "";
-				$seller_category_rawmaterial = "";
-				$seller_category_entertainment = "";
-				
-				$_POST['regtype']
-				$_POST['seller_email_value']
-*/
+
 		include 'core/init.php';
 		
-		//getting info from reg page to here
+		// to get the type from dropdown
 		$var_type=$_POST['regtype'];
-		$var_email= $_POST['seller_email_value']; //*****
-		$var_password=$_POST['seller_pwd'];
-		$var_first_name=$_POST['seller_first_name'];
-		$var_last_name=$_POST['seller_last_name'];
-		$var_bussiness_name=$_POST['seller_buss_name'];
+		echo $var_type;
+		
+		//getting info from reg page to here for seller
+		$var_seller_email= $_POST['seller_email_value']; //*****
+		echo $var_email;
+		$var_seller_password=$_POST['seller_pwd'];
+		$var_seller_first_name=$_POST['seller_first_name'];
+		$var_seller_last_name=$_POST['seller_last_name'];
+		$var_seller_bussiness_name=$_POST['seller_buss_name'];
 		$var_seller_addr=$_POST['seller_addr'];
+		$var_seller_mobile_number = $_POST ['seller_mobile_number']
 		$var_seller_rev_msg=$_POST['seller_rev_msg'];
 		$var_seller_category_food=$_POST['seller_category_food'];
 		$var_seller_category_electronics=$_POST['seller_category_electronics'];
 		$var_seller_category_rawmaterial=$_POST['seller_category_rawmaterial'];
 		$var_seller_category_entertainment=$_POST['seller_category_entertainment'];
-		//echo $var_category;
-	
+				
+		//getting info from reg page to here for buyer
+		
+		$var_buyer_email= $_POST['buyer_email']; //*****
+		$var_buyer_password=$_POST['buyer_pwd'];
+		$var_buyer_first_name=$_POST['buyer_first_name'];
+		$var_buyer_last_name=$_POST['buyer_last_name'];
+		$var_buyer_addr=$_POST['buyer_addr'];
+		$var_buyer_contact_no = $_POST ['buyer_mobile_number'];
+		
+		$var_buyer_rev_msg=$_POST['buyer_rev_msg'];
+		$var_buyer_category_food=$_POST['buyer_category_food'];
+		$var_buyer_category_electronics=$_POST['buyer_category_electronics'];
+		$var_buyer_category_rawmaterial=$_POST['buyer_category_rawmaterial'];
+		$var_buyer_category_entertainment=$_POST['buyer_category_entertainment'];
 
 		//inserting into db
-		$sql="insert into register (email,password,first_name,last_name,business_name,address,phone_number,category,type) values ('$var_email','$var_password','$var_first_name','$var_last_name','$var_bussiness_name','$var_address',$var_phone_number,'$var_category','$var_type')";
+		if ($var_type = 'seller')
+		{  // have to split the category based on the db 
+			$sql="insert into register (email,password,first_name,last_name,business_name,address,phone_number,receive_msg,category,type) values ('$var_seller_email','$var_seller_password','$var_seller_first_name','$var_seller_last_name','$var_seller_bussiness_name','$var_seller_addr',$var_seller_mobile_number,$var_seller_rev_msg,'$var_seller_category_food','$var_seller_type')";
+		}
+		else ($var_type = 'buyer')
+		{
+			$sql="insert into register (email,password,first_name,last_name,business_name,address,phone_number,receive_msg,category,type) values ('$var_buyer_email','$var_buyer_password','$var_buyer_first_name','$var_buyer_last_name','$var_buyer_bussiness_name','$var_buyer_addr',$var_buyer_mobile_number,$var_buyer_rev_msg,'$var_buyer_category_food','$var_buyer_type')";
+		}
 		if(mysqli_query($con,$sql))
 		{
 		echo "<script> alert(\"New record saved successfully..!\")</script>";
