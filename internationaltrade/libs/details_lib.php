b257cd34354d614711f81ad437d0e1ab741c71bc
kevincsuf <kevin.csuf@csu.fullerton.edu>
2016-04-10 16:36:52 -0700

    Improved validation of remaining stocks

--- internationaltrade/libs/details_lib.php
+++ internationaltrade/libs/details_lib.php
@@ -28,6 +28,29 @@ require_once("./login_lib.php");
 	echo "var_zipcode: " . $var_zipcode . "<br/>";
 	echo "var_closure_date: " . $var_closure_date . "<br/>";
 
+
+
+// Check remaining stocks again just before inserting the order to DB
+$order_quantity_query = "SELECT SUM(order_quantity) AS order_quantity_sum FROM join_deal where create_deal_id=".$var_create_deal_id;
+$order_quantity_result = mysqli_query($con, $order_quantity_query);
+$order_quantity_sum = 0;
+while($order_quantity_row = mysqli_fetch_array($order_quantity_result)) {
+    $order_quantity_sum += $order_quantity_row['order_quantity_sum'];
+}
+
+$deal_quantity_query = "SELECT * FROM create_deal WHERE deal_id=".$var_create_deal_id;
+$deal_quantity_result = mysqli_query($con, $deal_quantity_query);
+$deal_quantity_total = 0;
+while($deal_quantity_row = mysqli_fetch_array($deal_quantity_result)) {
+    $deal_quantity_total = $deal_quantity_row['max_quantity'];
+}
+
+$global_remaining_stocks = $deal_quantity_total - $order_quantity_sum;
+
+
+
+if ($global_remaining_stocks > 0) {
+
 	//inserting data into DB
 
 	$sql= "insert into join_deal(create_deal_id,user_id,order_quantity,address,state,zipcode,closure_date)values('$var_create_deal_id','$var_user_id','$var_order_quantity','$var_address','$var_state','$var_zipcode','$var_closure_date')";
@@ -44,7 +67,11 @@ require_once("./login_lib.php");
 	mysqli_close($con);
 	}
 
+}
 
+else {
+    echo "<script> alert(\"Currently out of stock!\")</script>";
+}
 
 
 ?>
